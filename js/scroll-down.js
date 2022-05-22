c97980d5e0083e38a336d71384dc5ee87359f549
DMU-P2614630 <73329121+DMU-P2614630@users.noreply.github.com>
2022-05-22 22:07:08 +0100

    Add files via upload

---
+++ js/scroll-down.js
@@ -0,0 +1,13 @@
+// hills animation
+  scene1.to("#h1-1", { y: 3 * speed, x: 1 * speed, scale: 0.9, ease: "power1.in" }, 0)
+  scene1.to("#h1-2", { y: 2.6 * speed, x: -0.6 * speed, ease: "power1.in" }, 0)
+  scene1.to("#h1-3", { y: 1.7 * speed, x: 1.2 * speed }, 0.03)
+  scene1.to("#h1-4", { y: 3 * speed, x: 1 * speed }, 0.03)
+  scene1.to("#h1-5", { y: 2 * speed, x: 1 * speed }, 0.03)
+  scene1.to("#h1-6", { y: 2.3 * speed, x: -2.5 * speed }, 0)
+  scene1.to("#h1-7", { y: 5 * speed, x: 1.6 * speed }, 0)
+  scene1.to("#h1-8", { y: 3.5 * speed, x: 0.2 * speed }, 0)
+  scene1.to("#h1-9", { y: 3.5 * speed, x: -0.2 * speed }, 0)
+
+  //animate text
+  scene1.to("#info", { y: 8 * speed }, 0)
